909be3c4af72de91b7de5d93b4dba83f2501f5b9
eremsimsek <eremsmsk@gmail.com>
2021-06-24 23:59:19 +0300

    Add: Scripts bird controls added Add: Prefabs created

---
+++ Assets/Scripts/BirdContreller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdContreller : MonoBehaviour
+{
+    public Rigidbody2D rgb;
+    public float JumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rgb.velocity = Vector2.up * JumpForce;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    public float minY;
+    public float maxY;
+    public float x;
+    public float interval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawn", interval, interval);
+    }
+
+    private void spawn()
+    {
+        GameObject instance = Instantiate(pipePrefab);
+        instance.transform.position = new Vector2(x, Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
